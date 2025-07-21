8b517f666b073a7e60cebe2e5f26a3aa1f363e50
ventiladordewolframio <68878309+ventiladordewolframio@users.noreply.github.com>
2025-07-20 21:58:02 -0300

    small changes, removed unecessary parts

--- src/app.js
+++ src/app.js
@@ -7,12 +7,10 @@ const PurchaseRouter = require('./routes/purchase');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use('/api/client', ClientRouter);
 app.use('/api/item', ItemRouter);
 app.use('/api/purchase', PurchaseRouter);
 
-// Starts the server
 app.listen(process.env.PORT, () => {
-  console.log(`The server is running at \"http://localhost:${process.env.PORT}\" !!!`);
+  console.log(`The server is running at \"${process.env.PORT}\"`);
 });
